c6de95ed61c53dc3fd7efbbc03a47be14d19b7ee
leiquanpan <32343702+leiquanpan@users.noreply.github.com>
2017-10-05 17:24:31 -0500

    Update pub.go

--- pub.go
+++ pub.go
@@ -15,7 +15,7 @@ func test(msg_count int,topic string){
   w, _ := nsq.NewProducer("18.221.119.174:4150", config)
   for i:=0 ; i<msg_count ; i++ {
     i_str := strconv.Itoa(i)
-    fmt.Println("message publish")
+    fmt.Println("message %d is publishing", i)
     err := w.Publish(topic, []byte("test"+i_str))
     if err != nil {
       log.Panic("Could not connect")
@@ -26,13 +26,19 @@ func test(msg_count int,topic string){
 }
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Please input number as well")
+  if len(os.Args) < 4 {
+    fmt.Println("Please input publisher number&number&topic as well")
   }
 
-  num, _ := strconv.Atoi(os.Args[1])
+  pub_num, _ := strconv.Atoi(os.Args[1])
+  num, _ := strconv.Atoi(os.Args[2])
+  topic := os.Args[3]
   
-  go test(num,"write_test")
+  for i := 0 ; i< pu_num ; i++ {
+    fmt.Printf("Right now is publisher %d \n",pub_num)
+    pub_str := strconv.Itoa(i)
+    go test(num,"write_test"+pub_str) 
+  }
 
   for{
     time.Sleep(1024 * time.Microsecond) 
