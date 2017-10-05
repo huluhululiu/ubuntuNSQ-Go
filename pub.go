58385e497554a81d1710fb6f3cb796fa50a1a7e2
leiquanpan <32343702+leiquanpan@users.noreply.github.com>
2017-10-05 16:21:59 -0500

    Update pub.go

--- pub.go
+++ pub.go
@@ -2,17 +2,34 @@ package main
 
 import (
   "log"
+  "fmt"
+  "os"
+  "strconv"
+  "time"
   "github.com/nsqio/go-nsq"
 )
 
-func main() {
+
+func test(msg_count,topic string){
   config := nsq.NewConfig()
   w, _ := nsq.NewProducer("18.221.119.174:4150", config)
-
-  err := w.Publish("write_test", []byte("test"))
-  if err != nil {
+  for i:=0 ; i<msg_count ; i++ {
+    err := w.Publish("write_test"+i, []byte("test "+i))
+    if err != nil {
       log.Panic("Could not connect")
+    }
+    time.Sleep(1024 * time.Microsecond)
   }
+}
+
+func main() {
+  if len(os.Args) < 2 {
+    fmt.Println("Please input number as well")
+  }
+
+  num, _ := strconv.Atoi(os.Args[1])
+  
+  go test(num,"test")
 
   w.Stop()
 }
