803ac5a91fe32c53a862c78190d9037a1dc3bf97
Hulu <liu433@wustl.edu>
2017-10-05 17:49:49 -0500

    subscriber

--- sub.go
+++ sub.go
@@ -3,17 +3,34 @@ package main
 import (
   "log"
   "sync"
-
-  "github.com/nsqio/go-nsq"
+  "time"
+  "os"
+  "runtime"
+  "strconv"
+  "github.com/bitly/go-nsq"
 )
+func test(msg_count int,topic string){
+  config := nsq.NewConfig()
+  w, _ := nsq.NewProducer("18.221.119.174:4150", config)
+  for i:=0 ; i<msg_count ; i++ {
+    i_str := strconv.Itoa(i)
+    fmt.Println("message is publishing", i)
+    err := w.Publish(topic, []byte("test"+i_str))
+    if err != nil {
+      log.Panic("Could not connect")
+    }
+    time.Sleep(1024 * time.Microsecond)
+  }
+  w.Stop()
+}
 
 func main() {
 
   wg := &sync.WaitGroup{}
   wg.Add(1)
-
+  topic := os.Args[1]
   config := nsq.NewConfig()
-  q, _ := nsq.NewConsumer("write_test", "ch", config)
+  q, _ := nsq.NewConsumer(topic, "ch", config)
   q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
       log.Printf("Got a message: %v", message)
       wg.Done()
