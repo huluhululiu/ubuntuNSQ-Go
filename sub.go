c6a678ddd8d82a9af19b3124c52755047df5a7b0
leiquanpan <32343702+leiquanpan@users.noreply.github.com>
2017-10-10 14:10:04 -0500

    Update sub.go

--- sub.go
+++ sub.go
@@ -9,22 +9,36 @@ import (
   "strconv"
   "github.com/bitly/go-nsq"
 )
-func main() {
 
-  wg := &sync.WaitGroup{}
-  wg.Add(1)
-  topic := os.Args[1]
+func test(numofmsg int, msg_size int, Channel string)
+{
+  //assign topic&channel, config consumer
+  channel := Channel + "m#ephemeral"
+  topic := channel
   config := nsq.NewConfig()
-  q, _ := nsq.NewConsumer(topic, "ch", config)
-  q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-      log.Printf("Got a message: %v", message)
-      wg.Done()
+  config.MaxInFlight = 1000000
+  config.OutputBufferSize = -1
+  sub, _ := nsq.NewConsumer(topic, channel, config)
+  
+  //design handler's handle function
+  sub.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+      HandleMsg(message.Body)
       return nil
   }))
-  err := q.ConnectToNSQD("18.221.119.174:4150")
+  
+  //connect
+  err := sub.ConnectToNSQD("18.221.119.174:4150")
   if err != nil {
       log.Panic("Could not connect")
   }
+   
+}
+
+func main() {
+  wg := &sync.WaitGroup{}
+  wg.Add(1)
+  topic := os.Args[1]
+  
   wg.Wait()
 
 }
